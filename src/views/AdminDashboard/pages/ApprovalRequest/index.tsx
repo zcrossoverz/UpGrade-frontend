f3ea81f1328f3db74a1460494db00ae6f5403e73
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2023-11-21 00:23:52 +0700

    feat: pagination approval request

--- src/views/AdminDashboard/pages/ApprovalRequest/index.tsx
+++ src/views/AdminDashboard/pages/ApprovalRequest/index.tsx
@@ -43,6 +43,23 @@ function ApprovalRequest() {
           ],
         }
       : {}),
+    ...(searchParams.get("status") !== null
+      ? {
+          explicit: [
+            {
+              key: "status",
+              value: searchParams.get("status"),
+            },
+          ],
+        }
+      : {
+          explicit: [
+            {
+              key: "status",
+              value: "Pending",
+            },
+          ],
+        }),
     order: {
       key: "id",
       value:
@@ -66,6 +83,7 @@ function ApprovalRequest() {
     paginationProps.pageSize,
     searchParams.get("search"),
     searchParams.get("order"),
+    searchParams.get("status"),
   ]);
 
   // end pagination
@@ -77,8 +95,8 @@ function ApprovalRequest() {
         <div className='flex justify-center items-center min-h-[200px] mt-4 w-full'>
           <Loader />
         </div>
-      ) : data?.count > 0 ? (
-        <>
+      ) : (
+        <div>
           <div className='flex mt-4'>
             <div className='border flex'>
               <input
@@ -115,146 +133,176 @@ function ApprovalRequest() {
                 <option value='ASC'>Cũ nhất</option>
               </select>
             </div>
+            <div className='ml-4 flex'>
+              <select
+                className='border px-2 py-1 text-sm pr-8'
+                onChange={(e) =>
+                  setSearchParams((prevSearchParams) => {
+                    const newParams = new URLSearchParams(prevSearchParams);
+                    newParams.set("status", e.target.value);
+                    return newParams;
+                  })
+                }
+              >
+                <option value='Pending'>Chưa duyệt</option>
+                <option value='Approved'>Chấp thuận</option>
+                <option value='Rejected'>Từ chối</option>
+              </select>
+            </div>
           </div>
-          <div className='flex justify-center mt-4 w-full'>
-            <Table
-              handleForward={(data: any) => {
-                navigate(`/admin/course-management/details/${data.course_id}`);
-              }}
-              handleTick={async (data: any) => {
-                setCourseId(data.course_id);
-                await approveRequestHook.mutateAsync({
-                  course_id: data.id,
-                  isAccept: true,
-                });
-              }}
-              handleCancel={async (data: any) => {
-                setCourseId(data.course_id);
-                await approveRequestHook.mutateAsync({
-                  course_id: data.id,
-                  isAccept: false,
-                });
-              }}
-              data={data?.datas?.map(
-                (
-                  e: {
-                    course_title: string;
-                    instructor_id: string;
-                    course_id: string;
-                    status: string;
-                    created_at: string;
-                    updated_at: string;
-                    id: number;
-                  },
-                  i: number
-                ) => ({
-                  order: (i + 1).toString(),
-                  course_title: e.course_title,
-                  updateDate: formatTime(e.updated_at),
-                  createDate: formatTime(e.created_at),
-                  status:
-                    (e.status === "Pending" && (
-                      <Badge
-                        text='Chờ duyệt'
-                        color='text-yellow-700'
-                        bgColor='bg-yellow-200'
-                      />
-                    )) ||
-                    (e.status === "Approved" && (
-                      <Badge
-                        text='Chấp thuận'
-                        color='text-green-700'
-                        bgColor='bg-green-200'
-                      />
-                    )) ||
-                    (e.status === "Rejected" && (
-                      <Badge
-                        text='Từ chối'
-                        color='text-red-700'
-                        bgColor='bg-red-200'
-                      />
-                    )),
-                  course_id: e.course_id,
-                  id: e.id,
-                })
-              )}
-              headerLabel={[
-                {
-                  key: "order",
-                  title: "#",
-                },
-                {
-                  key: "course_title",
-                  title: "Tiêu đề",
-                },
-                {
-                  key: "status",
-                  title: "Tình trạng",
-                },
-                {
-                  key: "updateDate",
-                  title: "CẬP NHẬT LÚC",
-                },
-                {
-                  key: "createDate",
-                  title: "KHỞI TẠO LÚC",
-                },
-              ]}
-            />
-          </div>
-          <Pagination
-            {...paginationProps}
-            next={() => {
-              setSearchParams((prevSearchParams) => {
-                const newParams = new URLSearchParams(prevSearchParams);
-                newParams.set("page", `${paginationProps.currentPage + 1}`);
-                return newParams;
-              });
-              setPagination((prev) => ({
-                ...prev,
-                currentPage: Number(prev.currentPage) + 1,
-              }));
-            }}
-            prev={() => {
-              setSearchParams((prevSearchParams) => {
-                const newParams = new URLSearchParams(prevSearchParams);
-                newParams.set("page", `${paginationProps.currentPage - 1}`);
-                return newParams;
-              });
-              setPagination((prevData) => ({
-                ...prevData,
-                currentPage: Number(prevData.currentPage) - 1,
-              }));
-            }}
-            goTo={(page: number) => {
-              setSearchParams((prevSearchParams) => {
-                const newParams = new URLSearchParams(prevSearchParams);
-                newParams.set("page", `${Number(page)}`);
-                return newParams;
-              });
-              setPagination((prevData) => ({
-                ...prevData,
-                currentPage: page,
-              }));
-            }}
-            pageSizeChange={(pageSize: number) => {
-              setSearchParams((prevSearchParams) => {
-                const newParams = new URLSearchParams(prevSearchParams);
-                newParams.set("pageSize", `${Number(pageSize)}`);
-                newParams.set("page", `1`);
-                return newParams;
-              });
-              setPagination((prev) => ({
-                ...prev,
-                pageSize,
-                page: 1,
-              }));
-            }}
-          />
-        </>
-      ) : (
-        <div className='flex justify-center items-center min-h-[200px] mt-4 w-full'>
-          <p className='text-gray-600'>Chưa có nội dung nào</p>
+          <>
+            {data?.count > 0 ? (
+              <>
+                <div className='flex justify-center mt-4 w-full'>
+                  <Table
+                    handleForward={(data: any) => {
+                      navigate(
+                        `/admin/course-management/details/${data.course_id}`
+                      );
+                    }}
+                    handleTick={async (data: any) => {
+                      setCourseId(data.course_id);
+                      await approveRequestHook.mutateAsync({
+                        course_id: data.id,
+                        isAccept: true,
+                      });
+                    }}
+                    handleCancel={async (data: any) => {
+                      setCourseId(data.course_id);
+                      await approveRequestHook.mutateAsync({
+                        course_id: data.id,
+                        isAccept: false,
+                      });
+                    }}
+                    data={data?.datas?.map(
+                      (
+                        e: {
+                          course_title: string;
+                          instructor_id: string;
+                          course_id: string;
+                          status: string;
+                          created_at: string;
+                          updated_at: string;
+                          id: number;
+                        },
+                        i: number
+                      ) => ({
+                        order: (i + 1).toString(),
+                        course_title: e.course_title,
+                        updateDate: formatTime(e.updated_at),
+                        createDate: formatTime(e.created_at),
+                        status:
+                          (e.status === "Pending" && (
+                            <Badge
+                              text='Chờ duyệt'
+                              color='text-yellow-700'
+                              bgColor='bg-yellow-200'
+                            />
+                          )) ||
+                          (e.status === "Approved" && (
+                            <Badge
+                              text='Chấp thuận'
+                              color='text-green-700'
+                              bgColor='bg-green-200'
+                            />
+                          )) ||
+                          (e.status === "Rejected" && (
+                            <Badge
+                              text='Từ chối'
+                              color='text-red-700'
+                              bgColor='bg-red-200'
+                            />
+                          )),
+                        course_id: e.course_id,
+                        id: e.id,
+                      })
+                    )}
+                    headerLabel={[
+                      {
+                        key: "order",
+                        title: "#",
+                      },
+                      {
+                        key: "course_title",
+                        title: "Tiêu đề",
+                      },
+                      {
+                        key: "status",
+                        title: "Tình trạng",
+                      },
+                      {
+                        key: "updateDate",
+                        title: "CẬP NHẬT LÚC",
+                      },
+                      {
+                        key: "createDate",
+                        title: "KHỞI TẠO LÚC",
+                      },
+                    ]}
+                  />
+                </div>
+                <Pagination
+                  {...paginationProps}
+                  next={() => {
+                    setSearchParams((prevSearchParams) => {
+                      const newParams = new URLSearchParams(prevSearchParams);
+                      newParams.set(
+                        "page",
+                        `${paginationProps.currentPage + 1}`
+                      );
+                      return newParams;
+                    });
+                    setPagination((prev) => ({
+                      ...prev,
+                      currentPage: Number(prev.currentPage) + 1,
+                    }));
+                  }}
+                  prev={() => {
+                    setSearchParams((prevSearchParams) => {
+                      const newParams = new URLSearchParams(prevSearchParams);
+                      newParams.set(
+                        "page",
+                        `${paginationProps.currentPage - 1}`
+                      );
+                      return newParams;
+                    });
+                    setPagination((prevData) => ({
+                      ...prevData,
+                      currentPage: Number(prevData.currentPage) - 1,
+                    }));
+                  }}
+                  goTo={(page: number) => {
+                    setSearchParams((prevSearchParams) => {
+                      const newParams = new URLSearchParams(prevSearchParams);
+                      newParams.set("page", `${Number(page)}`);
+                      return newParams;
+                    });
+                    setPagination((prevData) => ({
+                      ...prevData,
+                      currentPage: page,
+                    }));
+                  }}
+                  pageSizeChange={(pageSize: number) => {
+                    setSearchParams((prevSearchParams) => {
+                      const newParams = new URLSearchParams(prevSearchParams);
+                      newParams.set("pageSize", `${Number(pageSize)}`);
+                      newParams.set("page", `1`);
+                      return newParams;
+                    });
+                    setPagination((prev) => ({
+                      ...prev,
+                      pageSize,
+                      page: 1,
+                    }));
+                  }}
+                />
+              </>
+            ) : (
+              <div className='flex justify-center items-center min-h-[200px] mt-4 w-full'>
+                <p className='text-gray-600'>Chưa có nội dung nào</p>
+              </div>
+            )}
+          </>
         </div>
       )}
     </div>
